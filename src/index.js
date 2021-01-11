fc5e966d580bc27f1cecd34c69a7d2c648e28224
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-11 12:59:15 -0600

    2. JSX

--- src/index.js
+++ src/index.js
@@ -5,11 +5,32 @@
 
 // container.appendChild(element)
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges from react!</h1>
-const container = document.getElementById('app')
+// const jsx = <h1>Hello, Platzi Badges from react!</h1>;
+// const element = React.createElement('a', { href: 'http://platzi.com' }, 'Ir a platzi.')
+
+// const name = 'Eric'
+// const sum = () => 3 + 3
+// const element = React.createElement("h1", {}, `Hola soy ${name}`);
+// const jsx = <h1> Hola soy , {sum()} </h1>
+
+const jsx = (
+  <div>
+    <h1>Hola, soy Eric</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+)
+
+const element = React.createElement(
+  'div', 
+  {}, 
+  React.createElement('h1', {}, 'Hola, soy Eric'),
+  React.createElement('p', {}, 'Soy ingeniero de la web.')
+)
+
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__,__dónde__)
-ReactDOM.render(element,container)
+ReactDOM.render(element, container);
