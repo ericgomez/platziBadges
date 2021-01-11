6e838941b19b7bb28a66d8526a3267f896c7805a
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-11 15:27:09 -0600

    4.Estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,31 @@
 import React from 'react'
+
+import "./Styles/Badge.css"
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
   render() {
-    return <div>
-      <div>
+    return (
+    <div className="Badge">
+      <div className="Badge__header">
         <img src={confLogo} alt="Logo de la conferencia"/>
       </div>
 
-      <div>
-        <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+      <div className="Badge__section-name">
+        <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
         <h1>Eric <br/> Gomez</h1>
       </div>
 
-      <div>
-        <p>Fronted Engineer</p>
-        <p>@ericgomezmx</p>
+      <div className="Badge__section-info">
+        <h3>Fronted Engineer</h3>
+        <div>@ericgomezmx</div>
       </div>
 
-      <div>
+      <div className="Badge__footer">
         #platziconf
       </div>
     </div>
+    )
   }
 }
 
