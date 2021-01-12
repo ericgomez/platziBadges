5a12149e95f19963f91c3646a4a4695524564f81
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-12 16:50:36 -0600

    12.MejoranLaUIConUnLayout

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,7 +4,6 @@ import './styles/BadgeNew.css'
 import header from '../images/badge-header.svg'
 import Badge from '../components/Badge'
 import BadgeForm from '../components/BadgeForm'
-import Navbar from '../components/Navbar'
 
 class BadgeNew extends React.Component {
   
@@ -37,8 +36,7 @@ class BadgeNew extends React.Component {
 
   render() {
     return(
-      <div>
-        <Navbar />
+      <React.Fragment>
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo"/>
         </div>
@@ -52,7 +50,7 @@ class BadgeNew extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 }
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import './styles/Badges.css'
 import confLogo from '../images/badge-header.svg'
-import Navbar from '../components/Navbar'
 import BadgesList from '../components/BadgesList'
 import { Link } from 'react-router-dom'
 
@@ -42,34 +41,32 @@ class Badges extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
-
-      <div className="Badges">
-        <div className="Badges__hero">
-          <div className="Badges__container">
-            <img className="Badges_conf-logo" src={confLogo} alt="Conf Logo"/>
+      <React.Fragment>
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img className="Badges_conf-logo" src={confLogo} alt="Conf Logo"/>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="Badges__container">
-        <div className="Badges__buttons">
-          <Link to="/badges/new" className="btn btn-primary">
-            New Badges
-          </Link>
-        </div>
+        <div className="Badges__container">
+          <div className="Badges__buttons">
+            <Link to="/badges/new" className="btn btn-primary">
+              New Badges
+            </Link>
+          </div>
 
-        <div className="Badges__list">
-          <div className="Badges__container">
-          
-            <BadgesList badges={this.state.data} />
+          <div className="Badges__list">
+            <div className="Badges__container">
             
+              <BadgesList badges={this.state.data} />
+              
+            </div>
           </div>
-        </div>
 
-      </div>
-      </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
