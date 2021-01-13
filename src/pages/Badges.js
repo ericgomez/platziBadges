8e78954c467b6f72c12fe5441e67a094f03fb387
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-13 10:45:57 -0600

    14.APIRequestGET

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,78 +5,45 @@ import confLogo from '../images/badge-header.svg'
 import BadgesList from '../components/BadgesList'
 import { Link } from 'react-router-dom'
 
+import api from '../api'
+
 class Badges extends React.Component {
 
-  /*Este método se ejecuta cuando se instancia un componente. Nos permite definir el estado inicial del 		componente, hacer bind de métodos y definir propiedades internas en las que podemos guardar muchos datos diferente*/
-  constructor(props) {
-    super(props)
-    console.log('1. constructor()')
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  }; 
 
-    this.state = {
-      data: []
-    }
-  }
 
   /*Este último método de la fase de montado se ejecuta una vez el componente se renderizó en el navegador y nos permite interactuar con el DOM o las otras APIs del navegador (geolocation, navigator, notificaciones, etc.).*/
   componentDidMount() {
-    console.log('3. componentDidMount()')
-
-    this.timeOutId = setTimeout(() => {
-      this.setState({
-        data: [
-          {
-            id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-            irstName: "Freda",
-            lastName: "Grady",
-            email: "Leann_Berge@gmail.com",
-            jobTitle: "Legacy Brand Director",
-            twitter: "FredaGrady22221-7573",
-            avatarUrl: "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-          },
-          {
-            id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-            firstName: "Major",
-            lastName: "Rodriguez",
-            email: "Ilene66@hotmail.com",
-            jobTitle: "Human Research Architect",
-            twitter: "ajorRodriguez61545",
-            avatarUrl: "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-          },
-          {
-            id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-            firstName: "Daphney",
-            lastName: "Torphy",
-            email: "Ron61@hotmail.com",
-            jobTitle: "National Markets Officer",
-            twitter: "DaphneyTorphy96105",
-            avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-          }
-        ],
-      })
-    }, 3000);
+    this.fetchData()
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('5 componentDidUpdate()')
-    console.log({
-      prevProps: prevProps, prevState: prevState
-    })
-    console.log({
-      props: this.props,
-      state: this.state,
-    })
+  fetchData = async () => {
+    this.setState({ loading: true, error: null })
+
+    try {
+      const data = await api.badges.list()
+      this.setState({ loading: false, data: data })
+    } catch (error) {
+      this.setState({ loading: false, error: error })
+    }
   }
 
-  /*Este método se ejecuta cuando el componente se está por renderizar. En este punto es posible modificar el estado del componente sin causar una actualización (y por lo tanto no renderizar dos veces el componente).*/
-  componentWillUnmount() {
-    console.log('6. componentWillUnmount')
-
-    clearTimeout(this.timeOutId)
-  }
+  
 
   /*En este momento de la fase de montado se van a tomar las propiedades, el estado y el contexto y se va a generar la UI inicial de este componente*/
   render() {
-    console.log('2/4 render()')
+    if (this.state.loading === true) {
+      return 'Loading...'
+    }
+
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}`
+    }
+
     return (
       <React.Fragment>
         <div className="Badges">
