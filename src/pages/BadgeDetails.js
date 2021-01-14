15080498427aa004e6467fc92b6ac482631cd14c
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-14 12:37:44 -0600

    24.Hooks

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -6,7 +6,18 @@ import confLogo from '../images/platziconf-logo.svg'
 import Badge from '../components/Badge'
 import DeleteBadgeModal from '../components/DeleteBadgeModal'
 
+function useIncreaseCount(max) {
+  const [ count, setCount ] = React.useState(0)
+
+  if (count > max){
+    setCount(0);
+  }
+
+  return[count, setCount];
+}
+
 function BadgeDetails(props) {
+  const [ count, setCount ] = useIncreaseCount(4)
   //Obtenemos el valor del Badge enviado desde BadgeDetailsContainer
   const badge = props.badge
 
@@ -35,6 +46,12 @@ function BadgeDetails(props) {
               <h2>Actions</h2>
               <div>
                 <div>
+
+                  <button onClick={() => {
+                    setCount(count +1)
+                  }} className="btn btn-primary mr-4">
+                    Increase Count: {count}
+                  </button>
                   <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>
                     Edit
                   </Link>
